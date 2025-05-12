e3e5aabfeb8c3a59b0df44149a5bf0b5062b011b
Anthony Maciel <anthony.maciel@iatecam.org.br>
2025-05-12 19:11:50 -0400

    ci: adding ci check api

--- src/adapters/inbound/http/order/order.controller.spec.ts
+++ src/adapters/inbound/http/order/order.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderController } from './order.controller';
+import { PrismaService } from 'src/infrastructure/config/prisma/prisma.service';
+import { CreateOrderUseCase } from 'src/application/use-cases/create-order.usecase';
+import { OrderRepositoryPersistence } from 'src/infrastructure/persistence/prisma/order.repository.persistence';
 
 describe('OrderController', () => {
   let controller: OrderController;
@@ -7,6 +10,12 @@ describe('OrderController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrderController],
+      providers: [
+        PrismaService,
+        CreateOrderUseCase,
+        OrderRepositoryPersistence,
+      ],
+      imports: [],
     }).compile();
 
     controller = module.get<OrderController>(OrderController);
