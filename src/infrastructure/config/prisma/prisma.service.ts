6e3eb36d49ea103c81e8753e53f3910953c78f7e
Andre Costa <andreneoxgmail.com>
2025-05-21 21:31:58 -0400

    fix: run build

--- src/infrastructure/config/prisma/prisma.service.ts
+++ src/infrastructure/config/prisma/prisma.service.ts
@@ -1,17 +1,18 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaClientWithClient } from './prisma-extensions';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
   }
 
-  async enableShutdownHooks() {
-    this.$on('beforeExit', async () => {
-      await this.$disconnect();
-    });
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
 
