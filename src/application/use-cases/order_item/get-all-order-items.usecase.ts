e66355369489fb5714fa1d81411975637edfd15e
Anthony Maciel <anthony.maciel@iatecam.org.br>
2025-06-01 19:28:36 -0400

    refactor: prisma injector service

--- src/application/use-cases/order_item/get-all-order-items.usecase.ts
+++ src/application/use-cases/order_item/get-all-order-items.usecase.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PaginatedResult } from 'src/adapters/shared/repositories/repository.interface';
-import { OrderItem } from 'src/domain/entities/order_item/order-item.entity';
 import { OrderItemRepositoryPersistence } from 'src/infrastructure/persistence/prisma/order_item/order-item.repository.persistence';
-
-export type GetAllOrderItemsResponse = PaginatedResult<OrderItem>;
 
 @Injectable()
 export class GetAllOrderItemsUseCase {
@@ -11,12 +7,34 @@ export class GetAllOrderItemsUseCase {
     private readonly orderItemRepositoryPersistence: OrderItemRepositoryPersistence,
   ) {}
 
-  async execute(
-    limit: number = 10,
-    page: number = 1,
-  ): Promise<GetAllOrderItemsResponse> {
+  async execute(limit: number = 10, page: number = 1) {
     const skip = (page - 1) * limit;
 
-    return this.orderItemRepositoryPersistence.findAll(limit, skip);
+    const { currentPage, data, totalPages } =
+      await this.orderItemRepositoryPersistence.findAll(limit, skip);
+
+    return {
+      currentPage,
+      totalPages,
+      data: data.map((orderItem) => ({
+        id: orderItem.id.toString(),
+        orderId: orderItem.orderId.toString(),
+        notes: orderItem.notes,
+        productId: orderItem.productId,
+        quantity: orderItem.quantity,
+        product: {
+          id: orderItem.product.id.toString(),
+          name: orderItem.product.name,
+          description: orderItem.product.description,
+          price: orderItem.product.price,
+          imageUrl: orderItem.product.imageUrl,
+          category: {
+            id: orderItem.product.category.id.toString(),
+            name: orderItem.product.category.name,
+            description: orderItem.product.description,
+          },
+        },
+      })),
+    };
   }
 }
